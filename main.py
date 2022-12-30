10061cfb048486e3076ab337f152ecf1a52c1d72
georgasj <georgasj@ghotmail.com>
2022-12-30 22:17:20 +0200

    takes snapshot and extracts text but not perfect

--- main.py
+++ main.py
@@ -15,55 +15,61 @@ import cv2
 # import time
 import datetime
 from pytesseract import pytesseract
+from PIL import Image
 
 cv2.namedWindow("frame", 1)
 camera = cv2.VideoCapture(0)
 
-# Set the font and color
+# Set the font and color for text and roi rectangular
 font = cv2.FONT_HERSHEY_SIMPLEX
-color = (0, 0, 255)  # Blue
+color = (0, 0, 255)  # red due to BGR default type of image from openCV
 
-# Define the size of the region and the position of its top-left corner
-width = 650
-height = 650
-x = 400
-y = 400
+# Define the size of the roi w x h
+width = 450
+height = 450
+# and the position of text on the screen measuring from the top-left of the screen
+y = 100  # distance from top of screen
+x = 250  # distance from left of screen
 
 while True:
     # Capture a frame from the camera
     ret, frame = camera.read()
 
     # Convert the image to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    # gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-    # blur image
-    #blur = cv2.GaussianBlur(gray, (3, 3), 0)
+    # Convert the image from BGR to RGB
+    # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+    # blur image (to increase accuracy)
+    # blur = cv2.GaussianBlur(gray, (3, 3), 0)
 
     # Apply Otsu's threshold to binarize the image
     # threshold, _ = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
     # thresh = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
 
     # Morph open to remove noise and invert image
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
-    opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel, iterations=1)
-    invert = 255 - opening
-
-    # specify the roi (region of interest)
-    #roi = threshold[y:y + height, x:x + width]
+    # kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3, 3))
+    # opening = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel, iterations=1)
+    # invert = 255 - opening
 
     # frame = cv2.resize(frame, None, fx=1, fy=1)
     # frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
+    # Draw a rectangle on the image
+    cv2.rectangle(frame, (x, y), (x + width, y + height), (0, 255, 0), 2)
+    # specify the roi (region of interest)
+    roi = frame[y: y+height, x: x+width]
+
     # Perform text extraction
-    text = pytesseract.image_to_string(invert, lang='eng', config='--psm 6')
+    text = pytesseract.image_to_string(roi, lang='eng', config='--psm 6')
     # text = text.replace('\n', '').replace('\f', '')
 
-    cv2.putText(invert, text, (200, 200), font, 2, color, 2)
+    # this command places text on the frame at the position of x & y
+    cv2.putText(frame, text, ((y+width), (x+height)), font, 2, color, 2)
 
     # Display the frame along with the tesseract output
-    cv2.imshow("Frame", invert)
-    # cv2.imshow("Frame", opening)
-    # cv2.imshow("Frame", invert)
+    cv2.imshow("Frame", frame)
 
     key = cv2.waitKey(1)  # this must be 1 not 0
     if key == ord("p"):  # Press "p" to take a photo
@@ -74,7 +80,24 @@ while True:
         file_name = now.strftime("H%M%S") + ".png"
 
         # Save the photo with the timestamp as the file name
-        cv2.imwrite(file_name, invert)
+        cv2.imwrite(file_name, roi)
+
+        # Opening the image & storing it in an image object
+        image_path = '/Users/ygeorgas/mtgchk/'+file_name
+        img = Image.open(image_path)
+
+        # Passing the image object to image_to_string() function
+        img_text = pytesseract.image_to_string(img, config='--psm 6')
+        # text = pytesseract.image_to_string(img_text)
+
+        # Split the string into lines
+        # lines = img_text.split('\n')
+        # Print the first line
+        # print(lines[0], lines[1])
+        # print the whole text
+        print(img_text)
+        # print(text.replace('\n', '').replace('\f', ''))
+        # print(text[:-1])
 
     if key == ord("q"):  # Press "q" to quit
         break
@@ -82,37 +105,6 @@ while True:
 camera.release()
 cv2.destroyAllWindows()
 
-
-
-'''
-"extract text from snapshot"
-
-from PIL import Image
-from pytesseract import pytesseract
-
-
-# and the image we would be using
-image_path = "/Users/ygeorgas/mtgchk/card1.png"
-
-# Opening the image & storing it in an image object
-img = Image.open(image_path)
-
-# Providing the tesseract executable
-# location to pytesseract library
-#pytesseract.tesseract_cmd = path_to_tesseract
-
-# Passing the image object to image_to_string() function
-# This function will extract the text from the image
-text = pytesseract.image_to_string(img, config='--psm 6')
-#text = pytesseract.image_to_string(img)
-
-# Displaying the extracted text
-print(text)
-#print(text.replace('\n', '').replace('\f', ''))
-
-#print(text[:-1])
-
-'''
 '''
 # eBAY check price of MTG card
 
